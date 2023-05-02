4d1eac893fb2011287b94c54a608aa1603cdc264
David Girton <dgirton2@gmail.com>
2023-05-01 22:09:46 -0400

    update2

--- script.js
+++ script.js
@@ -18,6 +18,26 @@ console.log (score);
 
 }
 
+// set a countdown timer
+// start timer on page load 
 
 
+// stop timer on click of submit button
+
+const startingMinutes = 10;
+let time = startingMinutes * 60;
+
+const countDownEl = document.getElementById('countdown');
+
+setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+    const minutes = Math.floor(time/60);
+    let seconds = time % 60;
+
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    countDownEl.innerHTML = `${minutes}: ${seconds}`;
+    time--;
+}
 
